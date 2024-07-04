d99f58c33d8e23611cb9ef1aed9b8c2136f5cd7c
StiveLucas <161142347+StiveLucas@users.noreply.github.com>
2024-07-04 17:58:12 -0300

    Add files via upload

---
+++ Cor do botao.py
@@ -0,0 +1,5 @@
+#Comando de mudar a cor do botão
+style = discord.ButtonStyle.'cor desejada'
+
+#Exemplo de como ficaria
+ botao = discord.ui.Button(label = 'Botão', style = discord.ButtonStyle.green)
