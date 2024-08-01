b3de57af45741809bf2d3726f32139351ed9895a
StiveLucas <161142347+StiveLucas@users.noreply.github.com>
2024-08-01 08:07:39 -0300

    Delete Tutorias do bot directory

--- Tutorias do bot/Cor do botao.py
+++
@@ -1,5 +0,0 @@
-#Comando de mudar a cor do botão
-style = discord.ButtonStyle.'cor desejada'
-
-#Exemplo de como ficaria
- botao = discord.ui.Button(label = 'Botão', style = discord.ButtonStyle.green)
